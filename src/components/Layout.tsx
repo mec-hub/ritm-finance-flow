3769985e0ab430e6e8466e3f9fe427926136f176
mec-hub <rian.santos@aln.senaicimatec.edu.br>
2025-05-09 17:50:01 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-background flex w-full">
       <AppSidebar />
-      <main className="flex-1 p-4 md:p-6 lg:p-8 ml-16 md:ml-64 w-full">
+     <main className="flex-1 p-4 md:p-6 lg:p-8 pl-16 md:pl-64 w-full">
         <div className="max-w-[1600px] mx-auto w-full">
           {children}
         </div>
